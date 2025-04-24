b2d4170a4ee2d64be8b97fb904e281a341f56ccb
AsGamer1 <asilva.ys@gmail.com>
2025-04-24 18:12:04 +0200

    feat: update AndroidManifest, MainActivity, styles, and strings for splash screen and new architecture support

--- android/app/src/main/java/com/bseenmedia/bplayer/MainActivity.kt
+++ android/app/src/main/java/com/bseenmedia/bplayer/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.bseenmedia.bplayer
+import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
 import android.os.Bundle
@@ -10,7 +11,10 @@ import com.facebook.react.ReactActivity
 
 class MainActivity : ReactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
+        super.onCreate(null)
 
         // Ocultar barra de estado y de navegación
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
